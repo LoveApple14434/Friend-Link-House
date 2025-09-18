12480bd46253475301b068ff186f4f4fcfad585e
Harry <szy1985671808@163.com>
2025-09-18 19:58:07 +0800

    change data.json fetch url

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,7 +3,7 @@ import BlogInfoCard from "./components/BlogInfoCard.vue";
 import RSSUpdater from "./components/RSSUpdater.vue"; // 导入 RSS 更新组件
 import { ref, onMounted } from 'vue';
 
-const basicURL = 'https:://loveapple.icu/friend-link-house'
+const basicURL = '/friend-link-house'
 
 // 使用响应式数据
 const dataJson = ref([]);
